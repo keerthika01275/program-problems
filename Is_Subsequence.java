02a5a3626ae14e691c01eadce34a42e9c594f1f4
keerthika01275 <keerthika15122005@gmail.com>
2026-03-10 22:26:46 +0530

    new prblm from neet uploded

---
+++ Is_Subsequence.java
@@ -0,0 +1,17 @@
+public class Is_Subsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int i=0,j=0,m=s.length(),n=t.length();
+        while(i<m&&j<n){
+            char a = s.charAt(i);
+            char b = t.charAt(j);
+            if(a==b){
+                i++;
+                }
+            j++;
+        }
+        return i==m;
+    }
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("cat","cgfjahvt"));
+    }
+}
---
+++ Replace_element.java
@@ -0,0 +1,19 @@
+//You are given an array arr, replace every element in that array with t
+// he greatest element among the elements to its right,
+//  and replace the last element with -1.
+import java.util.*;
+public class Replace_element {
+    public static int[] replaceElements(int[] arr) {
+        int max = arr[arr.length-1];
+        arr[arr.length-1] =-1;
+        for(int i=arr.length-2;i>=0;i--){
+            int val = arr[i];
+            arr[i] = max;
+             if(max<val) max = val;
+        }
+        return arr;
+    }
+    public static void main(String args[]){
+        System.out.println(Arrays.toString(replaceElements(new int []{2,4,5,3,1,2})));
+    }
+}
